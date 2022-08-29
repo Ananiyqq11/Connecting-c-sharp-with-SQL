a45e933a9354c065c1f8b0264c2705c374ae33bc
Ananiyqq11 <107939492+Ananiyqq11@users.noreply.github.com>
2022-08-29 12:27:49 +0300

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExcer01
+{
+    class Item
+    {
+        public int Number { get; set; }
+        public string Date { get; set; }
+        public string Sku { get; set; }
+        public string Item_Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        static List<Item> _items = new List<Item>();
+        public static Item findone(String name)
+        {
+            return _items.Find(item => item.Item_Name == name);
+        }
+
+        //public String Count {get;set;}
+        
+
+
+        public void save()
+        {
+            _items.Add(this);
+            Console.WriteLine("Saved into the List(_items) Successfully");
+        }
+      static public List<Item> Getallproducts()
+        {
+            return _items;
+        }
+
+       
+
+    }
+
+}
